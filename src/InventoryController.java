a6066ae0f78dd2a8ecae30d39323d81055c59113
Ishuma Lahoti <ilahoti3@gatech.edu>
2020-10-04 15:57:14 -0400

    fix and finalize initial inventory

--- src/InventoryController.java
+++ src/InventoryController.java
@@ -1,45 +1,89 @@
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class InventoryController implements Initializable {
-    @FXML
-    GridPane gridPane = new GridPane();
+
     @FXML
     Label error;
+    @FXML
+    Label raspberrySeedQuantity;
+    @FXML
+    Label strawberrySeedQuantity;
+    @FXML
+    Label passionFruitSeedQuantity;
+    @FXML
+    Label grapeSeedQuantity;
 
-    Item[][] inventory = new Item[3][4]; //max 12 items in inventory for now. array version of inventory for list
+    final int capacity = 20;
+    int size = 4;
 
-    public void addItem(String s) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (inventory[i][j] == null) {
-                    Item item = new Item(s, 1);
-                    inventory[i][j] = item;
-                    Label item1 = new Label(item.getInfo());
-                    item1.setFont(new Font("Verdana", 11));
-                    item1.setTextFill(Color.web("#0076a3"));
-                    gridPane.add(item1, i, j, 1, 1);
-                    return;
-                }
-            }
-            error.setText("Your inventory is full-you cannot add more items");
+    //made add & remove for each item
+    public void addRaspberrySeed(int x) {
+        if (size == capacity) {
+            //for market, should have error message pop up when trying to buy
         }
+        int curr = Integer.parseInt(raspberrySeedQuantity.getText());
+        int val = curr + x;
+        raspberrySeedQuantity.setText("" + val);
+        size+=x;
+    }
+    public void removeRaspberrySeed() {
+        int curr = Integer.parseInt(raspberrySeedQuantity.getText());
+        int val = curr - 1;
+        raspberrySeedQuantity.setText("" + val);
+        size--;
+    }
+    public void addStrawberrySeed(int x) {
+        if (size == capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        int curr = Integer.parseInt(strawberrySeedQuantity.getText());
+        int val = curr + x;
+        strawberrySeedQuantity.setText("" + val);
+        size+=x;
+    }
+    public void removeStrawberrySeed() {
+        int curr = Integer.parseInt(strawberrySeedQuantity.getText());
+        int val = curr - 1;
+        strawberrySeedQuantity.setText("" + val);
+        size--;
+    }
+    public void addPassionFruitSeed(int x) {
+        if (size == capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        int curr = Integer.parseInt(passionFruitSeedQuantity.getText());
+        int val = curr + x;
+        passionFruitSeedQuantity.setText("" + val);
+        size+=x;
+    }
+    public void removePassionFruitSeed() {
+        int curr = Integer.parseInt(passionFruitSeedQuantity.getText());
+        int val = curr - 1;
+        passionFruitSeedQuantity.setText("" + val);
+        size--;
+    }
+    public void addGrapeSeed(int x) {
+        if (size == capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        int curr = Integer.parseInt(grapeSeedQuantity.getText());
+        int val = curr + x;
+       grapeSeedQuantity.setText("" + val);
+        size+=x;
+    }
+    public void removeGrapeSeed() {
+        int curr = Integer.parseInt(grapeSeedQuantity.getText());
+        int val = curr - 1;
+        grapeSeedQuantity.setText("" + val);
+        size--;
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        addItem("Strawberry Seed");
-        addItem("Raspberry Seed");
-        addItem("Passion Fruit Seed");
-        addItem("Grape Seed");
-
     }
 }
