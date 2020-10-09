e5970e1bae4a5a6f517b96e5a19501e020653003
Ankit Mehta <=>
2020-10-09 18:41:49 -0400

    Market FXMl changes

--- src/MarketController.java
+++ src/MarketController.java
@@ -18,26 +18,36 @@ public class MarketController implements Initializable {
     @FXML
     Label error;
     @FXML
-    Label raspberrySeed;
+    Label watermelonSeedP, pineappleSeedP, dragonFruitSeedP, beanStalkSeedP;
+
+    // These are buttons that will be clicked and the trolley will be filled.
     @FXML
-    Label strawberrySeed;
+    Label Buy;
     @FXML
-    Label passionFruitSeed;
-    @FXML
-    Label grapeSeed;
-    // TO get money and other valuable information from other screens.
+    Label Sell;
     @FXML
     private Game game = Game.factory();
 
+    //Listing of inventory and continual updating of the inventory.
+    // Listing of seeds to sell.
+    // Listing of seed to buy
 
     //The amount of seeds and capacity the market can hold
     final int marketCapacity = 100;
     final int size = 4;
+    @FXML
+    final double watermelonPrice = sellingPriceCalculator();
+    @FXML
+    final double pineapplePrice = sellingPriceCalculator();
+    @FXML
+    final double dragonFruitPrice = sellingPriceCalculator();
+    @FXML
+    final double beanStalkPrice = sellingPriceCalculator();
     final double BasePrice = 3.50;
     Random rand;
     int randomValue = rand.nextInt(4 ) + 1;
 
-    public double difficultyPrice() {
+    private double difficultyPrice() {
         if (game.getDifficulty().equals("Easy")) {
             return 2.0;
         } else if (game.getDifficulty().equals("Medium")) {
@@ -47,26 +57,34 @@ public class MarketController implements Initializable {
         }
     }
 
-    // the @param should be the action in which someone clicks a particular seed
-    // or crop to buy
-    public double sellingPriceCalculator() {
+    // you need to display the money next to the labels.
+    private double sellingPriceCalculator() {
+        // the price should be adjusted based on the seed.
         double sellPrice = BasePrice * difficultyPrice() + randomValue;
         return sellPrice;
     }
 
-    public double buyingPriceCalculator() {
+    private double buyingPriceCalculator() {
         double buyPrice = BasePrice * difficultyPrice() + (randomValue -1);
         return buyPrice;
     }
 
-
-    public void buy() {
-//        if (game.getMoney());
+    // the @param should be the action when someone wants to buy a crop or a seed.
+    @FXML
+    public void buy(ActionEvent event) throws IOException {
+        // Player's money - crop's selling price > 0
+       //if (game.getMoney() - );
+       //  update the player's inventory
+        // Update the player's money
     }
 
+    // the @param should be the action in which someone clicks a particular seed
+    // or crop to sell
     public void sell() {
-
+        // update the player's inventory
+        // update the player's money
     }
+
 
 
 
