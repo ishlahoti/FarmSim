20821ad209bdb3e1c7852f710c2aa8170c5d55c4
Ishuma Lahoti <ilahoti3@gatech.edu>
2020-10-09 18:32:13 -0400

    redo inventory

--- src/Game.java
+++ src/Game.java
@@ -6,6 +6,16 @@ public class Game {
     private String seed;
     private double money = 0.0D;
     private int day = 1;
+    private int raspberrySeedQuantity = 1;
+    private int strawberrySeedQuantity = 1;
+    private int passionFruitSeedQuantity = 1;
+    private int grapeSeedQuantity = 1;
+    private int strawberryCropQ;
+    private int raspberryCropQ;
+    private int passionFruitCropQ;
+    private int grapeCropQ;
+    final int capacity = 20;
+    private int size = 4;
 
     private Game() {
     }
@@ -64,5 +74,106 @@ public class Game {
 
     public int getDay() {
         return this.day;
+    }
+    public int getRaspberrySeedQuantity(){ return raspberrySeedQuantity;}
+    public int getStrawberrySeedQuantity(){ return strawberrySeedQuantity;}
+    public int getPassionFruitSeedQuantity() { return passionFruitSeedQuantity;}
+    public int getGrapeSeedQuantity() { return grapeSeedQuantity;}
+    public int getStrawberryCropQ() { return strawberryCropQ;}
+    public int getRaspberryCropQ(){return raspberryCropQ;}
+    public int getPassionFruitCropQ(){return passionFruitCropQ;}
+    public int getGrapeFruitCropQ() {return grapeCropQ;}
+
+
+
+
+    //made add & remove for each item in inventory
+    public void addRaspberrySeed(int x) {
+        if (size + x >= capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+       raspberrySeedQuantity+=x;
+        size+=x;
+    }
+    public void removeRaspberrySeed() {
+        raspberrySeedQuantity--;
+        size--;
+    }
+    public void addStrawberrySeed(int x) {
+        if (size + x >= capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        strawberrySeedQuantity+=x;
+        size+=x;
+    }
+    public void removeStrawberrySeed() {
+        strawberrySeedQuantity--;
+        size--;
+    }
+    public void addPassionFruitSeed(int x) {
+        if (size + x >= capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        passionFruitSeedQuantity+=x;
+        size+=x;
+    }
+    public void removePassionFruitSeed() {
+        passionFruitSeedQuantity--;
+        size--;
+    }
+    public void addGrapeSeed(int x) {
+        if (size + x >= capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        grapeSeedQuantity+=x;
+        size+=x;
+    }
+    public void removeGrapeSeed() {
+        grapeSeedQuantity--;
+        size--;
+    }
+    public void addStrawberryCrop(int x) {
+        if (size + x >= capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        strawberryCropQ+=x;
+        size+=x;
+    }
+    public void removeStrawberryCrop() {
+        strawberryCropQ--;
+        size--;
+    }
+    public void addRaspberryCrop(int x) {
+        if (size + x >= capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        raspberryCropQ+=x;
+        size+=x;
+    }
+    public void removeRaspberryCrop() {
+        raspberryCropQ--;
+        size--;
+    }
+    public void addPassionFruitCrop(int x) {
+        if (size + x >= capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        passionFruitCropQ+=x;
+        size+=x;
+    }
+    public void removePassionFruitCrop() {
+        passionFruitCropQ--;
+        size--;
+    }
+    public void addGrapeCrop(int x) {
+        if (size + x >= capacity) {
+            //for market, should have error message pop up when trying to buy
+        }
+        grapeCropQ+=x;
+        size+=x;
+    }
+    public void removeGrapeCrop() {
+        grapeCropQ--;
+        size--;
     }
 }
--- src/InventoryController.java
+++ src/InventoryController.java
@@ -24,8 +24,6 @@ public class InventoryController implements Initializable {
     @FXML
     Label grapeSeedQuantity;
     @FXML
-    Button farm;
-    @FXML
     Label strawberryCropQ;
     @FXML
     Label raspberryCropQ;
@@ -34,135 +32,18 @@ public class InventoryController implements Initializable {
     @FXML
     Label grapeCropQ;
 
-    final int capacity = 20;
-    int size = 4;
-
-    //made add & remove for each item
-    public void addRaspberrySeed(int x) {
-        if (size == capacity) {
-            //for market, should have error message pop up when trying to buy
-        }
-        int curr = Integer.parseInt(raspberrySeedQuantity.getText());
-        int val = curr + x;
-        raspberrySeedQuantity.setText("" + val);
-        size+=x;
-    }
-    public void removeRaspberrySeed() {
-        int curr = Integer.parseInt(raspberrySeedQuantity.getText());
-        int val = curr - 1;
-        raspberrySeedQuantity.setText("" + val);
-        size--;
-    }
-    public void addStrawberrySeed(int x) {
-        if (size == capacity) {
-            //for market, should have error message pop up when trying to buy
-        }
-        int curr = Integer.parseInt(strawberrySeedQuantity.getText());
-        int val = curr + x;
-        strawberrySeedQuantity.setText("" + val);
-        size+=x;
-    }
-    public void removeStrawberrySeed() {
-        int curr = Integer.parseInt(strawberrySeedQuantity.getText());
-        int val = curr - 1;
-        strawberrySeedQuantity.setText("" + val);
-        size--;
-    }
-    public void addPassionFruitSeed(int x) {
-        if (size == capacity) {
-            //for market, should have error message pop up when trying to buy
-        }
-        int curr = Integer.parseInt(passionFruitSeedQuantity.getText());
-        int val = curr + x;
-        passionFruitSeedQuantity.setText("" + val);
-        size+=x;
-    }
-    public void removePassionFruitSeed() {
-        int curr = Integer.parseInt(passionFruitSeedQuantity.getText());
-        int val = curr - 1;
-        passionFruitSeedQuantity.setText("" + val);
-        size--;
-    }
-    public void addGrapeSeed(int x) {
-        if (size == capacity) {
-            //for market, should have error message pop up when trying to buy
-        }
-        int curr = Integer.parseInt(grapeSeedQuantity.getText());
-        int val = curr + x;
-       grapeSeedQuantity.setText("" + val);
-        size+=x;
-    }
-    public void removeGrapeSeed() {
-        int curr = Integer.parseInt(grapeSeedQuantity.getText());
-        int val = curr - 1;
-        grapeSeedQuantity.setText("" + val);
-        size--;
-    }
-    public void addStrawberryCrop(int x) {
-        if (size == capacity) {
-            //for market, should have error message pop up when trying to buy
-        }
-        int curr = Integer.parseInt(strawberryCropQ.getText());
-        int val = curr + x;
-        strawberryCropQ.setText("" + val);
-        size+=x;
-    }
-    public void removeStrawberryCrop() {
-        int curr = Integer.parseInt(strawberryCropQ.getText());
-        int val = curr - 1;
-        strawberryCropQ.setText("" + val);
-        size--;
-    }
-    public void addRaspberryCrop(int x) {
-        if (size == capacity) {
-            //for market, should have error message pop up when trying to buy
-        }
-        int curr = Integer.parseInt(raspberryCropQ.getText());
-        int val = curr + x;
-        raspberryCropQ.setText("" + val);
-        size+=x;
-    }
-    public void removeRaspberryCrop() {
-        int curr = Integer.parseInt(raspberryCropQ.getText());
-        int val = curr - 1;
-        raspberryCropQ.setText("" + val);
-        size--;
-    }
-    public void addPassionFruitCrop(int x) {
-        if (size == capacity) {
-            //for market, should have error message pop up when trying to buy
-        }
-        int curr = Integer.parseInt(passionFruitCropQ.getText());
-        int val = curr + x;
-        passionFruitCropQ.setText("" + val);
-        size+=x;
-    }
-    public void removePassionFruitCrop() {
-        int curr = Integer.parseInt(passionFruitCropQ.getText());
-        int val = curr - 1;
-        passionFruitCropQ.setText("" + val);
-        size--;
-    }
-    public void addGrapeCrop(int x) {
-        if (size == capacity) {
-            //for market, should have error message pop up when trying to buy
-        }
-        int curr = Integer.parseInt(grapeCropQ.getText());
-        int val = curr + x;
-        grapeCropQ.setText("" + val);
-        size+=x;
-    }
-    public void removeGrapeCrop() {
-        int curr = Integer.parseInt(grapeCropQ.getText());
-        int val = curr - 1;
-        grapeCropQ.setText("" + val);
-        size--;
-    }
-
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Game game = Game.factory();
+        raspberrySeedQuantity.setText("" + game.getRaspberrySeedQuantity());
+        strawberrySeedQuantity.setText("" + game.getStrawberrySeedQuantity());
+        passionFruitSeedQuantity.setText("" + game.getPassionFruitSeedQuantity());
+        grapeSeedQuantity.setText("" + game.getGrapeSeedQuantity());
+        strawberryCropQ.setText("" + game.getStrawberryCropQ());
+        raspberryCropQ.setText("" + game.getRaspberryCropQ());
+        passionFruitCropQ.setText("" + game.getPassionFruitCropQ());
+        grapeCropQ.setText("" + game.getGrapeFruitCropQ());
     }
     @FXML
     private void changeScreen(ActionEvent event) throws IOException {
