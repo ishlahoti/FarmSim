841786cd0a68c4cae7baed667284855dc88a33f9
rishi-bhatnager <rishibhatnager01@gmail.com>
2020-10-13 14:53:25 -0400

    initialize plots randomly

--- src/IfarmuiController.java
+++ src/IfarmuiController.java
@@ -15,7 +15,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Popup;
+import sun.awt.image.ImageWatched;
 
 
 public class IfarmuiController implements Initializable {
@@ -27,6 +29,8 @@ public class IfarmuiController implements Initializable {
     private Label name;
     @FXML
     private Label text0, text1, text2, text3, text4, text5, text6, text7, text8, text9;
+    private Label[] plots;
+
     @FXML
     private GridPane grid;
 
@@ -61,8 +65,8 @@ public class IfarmuiController implements Initializable {
     @FXML
     private void harvest(ActionEvent event) throws IOException {
         List<Label> maturePlants = new LinkedList<>();
-        for (Node plotNode : grid.getChildren()) {
-            Label plot = (Label) plotNode;
+        for (Label plot : plots) {
+//            Label plot = (Label) plotNode;
 
             if (plot.getText().contains("Mature"))
                 maturePlants.add(plot);
@@ -70,79 +74,63 @@ public class IfarmuiController implements Initializable {
 
         for (Label maturePlant : maturePlants) {
             maturePlant.setText("Empty Plot");
+            // add to inventory
         }
 
 
         Label label;
 
-        if (maturePlants.isEmpty())
+        int numMature = maturePlants.size();
+        if (numMature == 0) {
             label = new Label("You don't have any mature plants to harvest!");
-        else
-            label = new Label("You harvested " + maturePlants.size() + " mature plants");
+        } else {
+            label = new Label("You harvested " + numMature + " mature plant" + (numMature == 1 ? "!" : "s!"));
+        }
 
-        // create a popup
         Popup popup = new Popup();
-
-        // set background
-        label.setStyle(" -fx-background-color: gray;");
-
-        // add the label
+        label.setStyle(" -fx-background-color: lightgray;");
         popup.getContent().add(label);
-
-        // set size of label
-        label.setMinWidth(80);
+        label.setMinWidth(250);
         label.setMinHeight(50);
-
+        label.setAlignment(Pos.CENTER);
         popup.show(Main.getPrimaryStage());
     }
 
-    /*
+
     @FXML
-    private static void initializePlants(){
-        StackPane plot0 = new StackPane(), plot1 = new StackPane(), plot2 = new StackPane(), plot3 = new StackPane();
-        StackPane plot4 = new StackPane(), plot5 = new StackPane(), plot6 = new StackPane(), plot7 = new StackPane();
-        StackPane plot8 = new StackPane(), plot9 = new StackPane();
+    private void initializePlants(){
+        String[] plantTypes = new String[3];
+        plantTypes[0] = "seed";
+        plantTypes[1] = "immature";
+        plantTypes[2] = "mature";
 
-        System.out.println(plot0 == null);
-        System.out.println("Style: " + plot0.getStyle());
-        System.out.println("Style Class: " + plot0.getStyleClass());
+        for (Label plot : plots) {
+            String type = plantTypes[(new Random()).nextInt(plantTypes.length)];
 
-        System.out.println(plot0.getChildren());
-        plot0.getChildren().add(new Label("text"));
-        plot0.getChildren().add(new ImageView("https://www.jing.fm/clipimg/full/20-200139_28-collection-of-apple-seed-clipart-cars-jeans.png"));
-        System.out.println(plot0.getChildren());
-        plot0.getChildren().get(1).setVisible(true);
-
-
-//        String[] plantTypes = new String[3];
-//        plantTypes[0] = "seedImage";
-//        plantTypes[1] = "immaturePlantImage";
-//        plantTypes[2] = "maturePlantImage";
-//
-//        for (StackPane plot : plots.getChildren()) {
-//            plot.setStyle(plantTypes[(new Random()).nextInt(plantTypes.length)]);
-//        }
+            switch (type) {
+                case "seed":
+                    plot.setText(this.game.getSeed() + " Seed");
+                    break;
+                case "immature":
+                    plot.setText("Immature " + this.game.getSeed() + " Plant");
+                    break;
+                case "mature":
+                    plot.setText("Mature " + this.game.getSeed() + " Plant");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-     */
+
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.day.setText("Day " + this.game.getDay());
         this.name.setText(this.game.getName() + "'s Farm");
-
-        this.text0.setText(this.game.getSeed() + " Seed");
-        this.text1.setText("Mature " + this.game.getSeed() + " Plant");
-        this.text2.setText("Immature " + this.game.getSeed() + " Plant");
-        this.text3.setText("Immature " + this.game.getSeed() + " Plant");
-        this.text4.setText(this.game.getSeed() + " Seed");
-
-        this.text5.setText("Mature " + this.game.getSeed() + " Plant");
-        this.text6.setText("Immature " + this.game.getSeed() + " Plant");
-        this.text7.setText(this.game.getSeed() + " Seed");
-        this.text8.setText(this.game.getSeed() + " Seed");
-        this.text9.setText("Mature " + this.game.getSeed() + " Plant");
-
         this.difficultyLabel();
-//        this.initializePlants();
+
+        plots =  new Label[] {text0,text1,text2,text3,text4,text5,text6,text7,text8,text9};
+        this.initializePlants();
     }
 }
 
