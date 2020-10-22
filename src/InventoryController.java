434ad5815eca9aee91f0eaf99d31ab39e11aaf13
Ishuma Lahoti <ilahoti3@gatech.edu>
2020-10-22 12:25:20 -0400

    implement planting for all seeds

--- src/InventoryController.java
+++ src/InventoryController.java
@@ -97,31 +97,168 @@ public class InventoryController implements Initializable {
         Label label = new Label();
         Popup popup = new Popup();
         String[] plots = farm.getPlots();
+        boolean empty = false;
         switch (((Button) event.getSource()).getId()) {
             case "raspberryPlant" :
                 if (game.getRaspberrySeedQuantity() > 0) {
-                    game.addRaspberrySeed(-1); //reduce quantity
-                    raspberrySeedQuantity.setText("" + game.getRaspberrySeedQuantity());
-                    inventoryCapacity.setText("Inventory Capacity: "
-                            + game.getTotalInventoryQuantity() + "/"
-                            + game.getInventoryCapacity());
-                    label.setText("You planted one Raspberry Seed!");
                     for (int i = 0; i < plots.length; i++) {
                         String text = plots[i];;
                         if (text.compareTo("Empty") == 0) {
+                            label.setText("You planted one Raspberry Seed!");
                             farm.editPlot(i, "Raspberry seed");
+                            empty = true;
+                            game.addRaspberrySeed(-1); //reduce quantity
+                            raspberrySeedQuantity.setText("" + game.getRaspberrySeedQuantity());
                             break;
                         }
                     }
                 }
                 else {
+                    empty = true;
                     label.setText("Oops! You don't have enough raspberry seeds to plant. Visit the market!");
             }
                 break;
             case "strawberryPlant" :
-
+                if (game.getStrawberrySeedQuantity() > 0) {
+                    for (int i = 0; i < plots.length; i++) {
+                        String text = plots[i];;
+                        if (text.compareTo("Empty") == 0) {
+                            label.setText("You planted one Strawberry Seed!");
+                            farm.editPlot(i, "Strawberry seed");
+                            empty = true;
+                            game.addStrawberrySeed(-1); //reduce quantity
+                            strawberrySeedQuantity.setText("" + game.getStrawberrySeedQuantity());
+                            break;
+                        }
+                    }
+                }
+                else {
+                    empty = true;
+                    label.setText("Oops! You don't have enough strawberry seeds to plant. Visit the market!");
+                }
+                break;
+            case "passionPlant" :
+                if (game.getPassionFruitSeedQuantity() > 0) {
+                    for (int i = 0; i < plots.length; i++) {
+                        String text = plots[i];;
+                        if (text.compareTo("Empty") == 0) {
+                            label.setText("You planted one Passion Fruit Seed!");
+                            farm.editPlot(i, "Passion Fruit seed");
+                            empty = true;
+                            game.addPassionFruitSeed(-1); //reduce quantity
+                            passionFruitSeedQuantity.setText("" + game.getPassionFruitSeedQuantity());
+                            break;
+                        }
+                    }
+                }
+                else {
+                    empty = true;
+                    label.setText("Oops! You don't have enough passion fruit seeds to plant. Visit the market!");
+                }
+                break;
+            case "grapePlant" :
+                if (game.getGrapeSeedQuantity() > 0) {
+                    for (int i = 0; i < plots.length; i++) {
+                        String text = plots[i];;
+                        if (text.compareTo("Empty") == 0) {
+                            label.setText("You planted one Grape Seed!");
+                            farm.editPlot(i, "Grape seed");
+                            empty = true;
+                            game.addGrapeSeed(-1); //reduce quantity
+                            grapeSeedQuantity.setText("" + game.getGrapeSeedQuantity());
+                            break;
+                        }
+                    }
+                }
+                else {
+                    empty = true;
+                    label.setText("Oops! You don't have enough grape seeds to plant. Visit the market!");
+                }
+                break;
+            case "watermelonPlant" :
+                if (game.getWatSeedQ() > 0) {
+                    for (int i = 0; i < plots.length; i++) {
+                        String text = plots[i];;
+                        if (text.compareTo("Empty") == 0) {
+                            label.setText("You planted one Watermelon Seed!");
+                            farm.editPlot(i, "Watermelon seed");
+                            empty = true;
+                            game.addWatSeed(-1); //reduce quantity
+                            watSeedQ.setText("" + game.getWatSeedQ());
+                            break;
+                        }
+                    }
+                }
+                else {
+                    empty = true;
+                    label.setText("Oops! You don't have enough watermelon seeds to plant. Visit the market!");
+                }
+                break;
+            case "pineapplePlant" :
+                if (game.getPinSeedQ() > 0) {
+                    for (int i = 0; i < plots.length; i++) {
+                        String text = plots[i];;
+                        if (text.compareTo("Empty") == 0) {
+                            label.setText("You planted one Pineapple Seed!");
+                            farm.editPlot(i, "Pineapple seed");
+                            empty = true;
+                            game.addPinSeed(-1); //reduce quantity
+                            pinSeedQ.setText("" + game.getPinSeedQ());
+                            break;
+                        }
+                    }
+                }
+                else {
+                    empty = true;
+                    label.setText("Oops! You don't have enough pineapple seeds to plant. Visit the market!");
+                }
+                break;
+            case "dragonPlant" :
+                if (game.getDragSeedQ() > 0) {
+                    for (int i = 0; i < plots.length; i++) {
+                        String text = plots[i];;
+                        if (text.compareTo("Empty") == 0) {
+                            label.setText("You planted one Dragon Fruit Seed!");
+                            farm.editPlot(i, "Dragon Fruit seed");
+                            empty = true;
+                            game.addDragSeed(-1); //reduce quantity
+                            dragSeedQ.setText("" + game.getDragSeedQ());
+                            break;
+                        }
+                    }
+                }
+                else {
+                    empty = true;
+                    label.setText("Oops! You don't have enough dragon fruit seeds to plant. Visit the market!");
+                }
+                break;
+            case "beanPlant" :
+                if (game.getBeanSeedQ() > 0) {
+                    for (int i = 0; i < plots.length; i++) {
+                        String text = plots[i];;
+                        if (text.compareTo("Empty") == 0) {
+                            label.setText("You planted one Bean Stalk Seed!");
+                            farm.editPlot(i, "Bean Stalk seed");
+                            empty = true;
+                            game.addBeanSeed(-1); //reduce quantity
+                            beanSeedQ.setText("" + game.getBeanSeedQ());
+                            break;
+                        }
+                    }
+                }
+                else {
+                    empty = true;
+                    label.setText("Oops! You don't have enough bean stalk seeds to plant. Visit the market!");
+                }
+                break;
         }
-
+        //outside switch
+        inventoryCapacity.setText("Inventory Capacity: "
+                + game.getTotalInventoryQuantity() + "/"
+                + game.getInventoryCapacity());
+        if (! empty) {
+            label.setText("Oops! You don't have any empty plot spaces. Harvest some crops!");
+        }
         label.setText(label.getText() + "\nClick out of this box to exit");
         label.setStyle(" -fx-background-color: lightgray;");
         popup.getContent().add(label);
