468f1e08f8159c927738ff99e3a261ab710fc379
Ishuma Lahoti <ilahoti3@gatech.edu>
2020-10-22 17:06:57 -0400

    shorten plant method

--- src/InventoryController.java
+++ src/InventoryController.java
@@ -94,10 +94,9 @@ public class InventoryController implements Initializable {
     }
     @FXML
     private void plant(ActionEvent event) {
-        Label label = new Label();
+        Label label = new Label("");
         Popup popup = new Popup();
         String[] plots = farm.getPlots();
-        boolean empty = false;
         switch (((Button) event.getSource()).getId()) {
         case "raspberryPlant" :
             if (game.getRaspberrySeedQuantity() > 0) {
@@ -106,14 +105,12 @@ public class InventoryController implements Initializable {
                     if (text.compareTo("Empty") == 0) {
                         label.setText("You planted a Raspberry Seed!");
                         farm.editPlot(i, "Raspberry Seed");
-                        empty = true;
                         game.addRaspberrySeed(-1); //reduce quantity
                         raspberrySeedQuantity.setText("" + game.getRaspberrySeedQuantity());
                         break;
                     }
                 }
             } else {
-                empty = true;
                 label.setText("You don't have any raspberry seeds to plant. Visit the market!");
             }
             break;
@@ -124,14 +121,12 @@ public class InventoryController implements Initializable {
                     if (text.compareTo("Empty") == 0) {
                         label.setText("You planted a Strawberry Seed!");
                         farm.editPlot(i, "Strawberry Seed");
-                        empty = true;
                         game.addStrawberrySeed(-1); //reduce quantity
                         strawberrySeedQuantity.setText("" + game.getStrawberrySeedQuantity());
                         break;
                     }
                 }
             } else {
-                empty = true;
                 label.setText("You don't have any strawberry seeds to plant. Visit the market!");
             }
             break;
@@ -142,14 +137,12 @@ public class InventoryController implements Initializable {
                     if (text.compareTo("Empty") == 0) {
                         label.setText("You planted a Passion Fruit Seed!");
                         farm.editPlot(i, "Passion Fruit Seed");
-                        empty = true;
                         game.addPassionFruitSeed(-1); //reduce quantity
                         passionFruitSeedQuantity.setText("" + game.getPassionFruitSeedQuantity());
                         break;
                     }
                 }
             } else {
-                empty = true;
                 label.setText("You don't have any passion fruit seeds to plant. Visit the market!");
             }
             break;
@@ -160,14 +153,12 @@ public class InventoryController implements Initializable {
                     if (text.compareTo("Empty") == 0) {
                         label.setText("You planted a Grape Seed!");
                         farm.editPlot(i, "Grape Seed");
-                        empty = true;
                         game.addGrapeSeed(-1); //reduce quantity
                         grapeSeedQuantity.setText("" + game.getGrapeSeedQuantity());
                         break;
                     }
                 }
             } else {
-                empty = true;
                 label.setText("You don't have any grape seeds to plant. Visit the market!");
             }
             break;
@@ -178,14 +169,12 @@ public class InventoryController implements Initializable {
                     if (text.compareTo("Empty") == 0) {
                         label.setText("You planted a Watermelon Seed!");
                         farm.editPlot(i, "Watermelon Seed");
-                        empty = true;
                         game.addWatSeed(-1); //reduce quantity
                         watSeedQ.setText("" + game.getWatSeedQ());
                         break;
                     }
                 }
             } else {
-                empty = true;
                 label.setText("You don't have any watermelon seeds to plant. Visit the market!");
             }
             break;
@@ -196,14 +185,12 @@ public class InventoryController implements Initializable {
                     if (text.compareTo("Empty") == 0) {
                         label.setText("You planted a Pineapple Seed!");
                         farm.editPlot(i, "Pineapple Seed");
-                        empty = true;
                         game.addPinSeed(-1); //reduce quantity
                         pinSeedQ.setText("" + game.getPinSeedQ());
                         break;
                     }
                 }
             } else {
-                empty = true;
                 label.setText("You don't have any pineapple seeds to plant. Visit the market!");
             }
             break;
@@ -214,14 +201,12 @@ public class InventoryController implements Initializable {
                     if (text.compareTo("Empty") == 0) {
                         label.setText("You planted a Dragon Fruit Seed!");
                         farm.editPlot(i, "Dragon Fruit Seed");
-                        empty = true;
                         game.addDragSeed(-1); //reduce quantity
                         dragSeedQ.setText("" + game.getDragSeedQ());
                         break;
                     }
                 }
             } else {
-                empty = true;
                 label.setText("You don't have any dragon fruit seeds to plant. Visit the market!");
             }
             break;
@@ -232,34 +217,26 @@ public class InventoryController implements Initializable {
                     if (text.compareTo("Empty") == 0) {
                         label.setText("You planted a Bean Stalk Seed!");
                         farm.editPlot(i, "Bean Stalk Seed");
-                        empty = true;
                         game.addBeanSeed(-1); //reduce quantity
                         beanSeedQ.setText("" + game.getBeanSeedQ());
                         break;
                     }
                 }
             } else {
-                empty = true;
                 label.setText("You don't have any bean stalk seeds to plant. Visit the market!");
             }
             break;
         default :
             break;
         }
-        inventoryCapacity.setText("Inventory Capacity: "
-                + game.getTotalInventoryQuantity() + "/"
+        inventoryCapacity.setText("Inventory Capacity: " + game.getTotalInventoryQuantity() + "/"
                 + game.getInventoryCapacity());
-        if (!empty) {
+        if (label.getText().equals("")) {
             label.setText("You don't have any empty plot spaces. Harvest some crops!");
         }
-
-        label.setText(label.getText() + "\nClick out of this box to exit");
         label.setStyle(" -fx-background-color: lightgray;");
         popup.getContent().add(label);
-        label.setMinWidth(250);
-        label.setMinHeight(50);
         label.setAlignment(Pos.CENTER);
-        label.setTextAlignment(TextAlignment.CENTER);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         popup.show(window);
         popup.setAutoHide(true);
