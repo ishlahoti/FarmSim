95aa2f0fb354ba41806e0b38ebc222013de0010c
Anshul <atusnial3@gatech.edu>
2020-10-22 20:28:32 -0400

    Fixed checkstyle

--- src/InventoryController.java
+++ src/InventoryController.java
@@ -8,7 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 
